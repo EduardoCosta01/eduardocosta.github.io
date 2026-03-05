d0f08f57a7103ff9de4c251b7351a567a0fabc90
Eduardo Costa01 <eccosta2000@bol.com.br>
2026-03-04 22:14:55 -0300

    projeto completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,27 @@ function changeTheme() {
     toggleTheme.classList.toggle('bi-sun');
     toggleTheme.classList.toggle('bi-moon-stars');
 
-
 }
 
-
 toggleTheme.addEventListener('click',changeTheme )
+
+const accordionHeaders = document.querySelectorAll('.accordion__header');
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
+    })
+})
+
+const menuLinks = document.querySelectorAll('.menu__Links');
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  })
+})
+
+
